ab6b228988e163e2763d8342ead6c77d018a3f2d
Chris Ixion <chris@planktin.com>
2017-03-07 16:56:20 +1100

    mount ShopifyApp engine at /shopify

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-  mount ShopifyApp::Engine, at: '/'
+  mount ShopifyApp::Engine, at: '/shopify'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
