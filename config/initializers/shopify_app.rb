d5a4274f1be2382a3d0e6eb13435e59b65e9fcc5
Chris Ixion <chris@planktin.com>
2017-03-09 07:55:59 +1100

    use ENV.fetch to raise KeyError for missing ENV variables

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,11 +1,11 @@
 ShopifyApp.configure do |config|
   config.application_name = "Plankify"
-  config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
-  config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
+  config.api_key = ENV.fetch('SHOPIFY_CLIENT_API_KEY')
+  config.secret = ENV.fetch('SHOPIFY_CLIENT_API_SECRET')
   config.scope = "read_orders, read_products, write_orders, write_products, read_customers, write_customers"
   config.embedded_app = true
 
   config.webhooks = [
-    {topic: "products/update", address: "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com/webhooks/products_update", format: "json"},
+    {topic: "products/update", address: "https://#{ENV.fetch('HEROKU_APP_NAME')}.herokuapp.com/webhooks/products_update", format: "json"},
   ]
 end
