39c9df0e7f62620fa28869293f569508a0ab3d2f
Chris Ixion <chris@planktin.com>
2017-03-08 13:06:59 +1100

    add products update webhook via generator

--- app/jobs/products_update_job.rb
+++ app/jobs/products_update_job.rb
@@ -1,8 +1,9 @@
 class ProductsUpdateJob < ActiveJob::Base
-  queue_as :default
+  def perform(shop_domain:, webhook:)
+    shop = Shop.find_by(shopify_domain: shop_domain)
 
-  def perform(*args)
-    # Do something later
-    puts "Job triggered, check logs"
+    shop.with_shopify_session do
+      puts "Triggering job for: #{shop.shopify_domain}"
+    end
   end
 end
